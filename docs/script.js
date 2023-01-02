815b49f22f98d1953c9ebbe1e43643713cf89c63
dev <121394537+sockthedeveloper@users.noreply.github.com>
2023-01-02 16:37:08 +0000

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,12 @@
 
+var i = 0;
+var txt = 'The best customizable biolink service of 2023';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
